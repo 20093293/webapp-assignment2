e7c2d4f0701ece4062002289e10239d2b0d10748
Conor Grace <20093293@mail.wit.ie>
2023-11-25 18:32:37 +0000

    Fixed API error

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -81,7 +81,7 @@ export const getMovies = () => {
 
   export const getTopRatedMovies = () => {
     return fetch(
-      `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_TMDB_KEY}language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
     ).then(res => res.json())
     .then(json => json.results);
     };
