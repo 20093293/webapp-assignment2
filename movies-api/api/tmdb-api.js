0b2be5554b6dc69153c3c96b085afa88b25a1312
Conor Grace <20093293@mail.wit.ie>
2023-12-22 16:43:53 +0000

    Changes

---
+++ movies-api/api/tmdb-api.js
@@ -0,0 +1,131 @@
+import fetch from 'node-fetch';
+
+export const getUpcomingMovies = async () => {
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+        );
+
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getMovie = (args) => {
+  // console.log(args)
+  const [, idPart] = args.queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    throw error
+ });
+};
+
+export const getGenres = async () => {
+    return fetch(
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+        process.env.TMDB_KEY +
+        "&language=en-US"
+    ).then( (response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
+
+  export const getMovieImages = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then( (response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+  
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
+  export const getMovieReviews = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        return json.results;
+      });
+  };
+
+  export const getTopRatedMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    ).then(res => res.json())
+    .then(json => json.results);
+    };
+
+    export const getTopActors = () => {
+      return fetch(
+        `https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+      )
+        .then((res) => res.json())
+        .then((json) => json.results);
+    };
+
+    export const getMovieCredits = (id) => {
+      return fetch(
+        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+      )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch movie credits');
+        }
+        return response.json();
+      })
+      .then((json) => {
+        return json.cast; 
+      })
+      .catch((error) => {
+        throw error;
+      });
+    };
+
+    export const login = async (username, password) => {
+      const response = await fetch('http://localhost:8080/api/users', {
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          method: 'post',
+          body: JSON.stringify({ username: username, password: password })
+      });
+      return response.json();
+  };
+  
+  export const signup = async (username, password) => {
+      const response = await fetch('http://localhost:8080/api/users?action=register', {
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          method: 'post',
+          body: JSON.stringify({ username: username, password: password })
+      });
+      return response.json();
+  };
